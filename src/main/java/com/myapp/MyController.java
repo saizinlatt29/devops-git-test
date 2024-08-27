8d395e044bedca2d0ba9d9b30023a2dc5db4b510
[abc] <saizin.latt.it@gmail.com>
2024-08-27 13:45:42 -0700

    Testing finished

---
+++ src/main/java/com/myapp/MyController.java
@@ -0,0 +1,12 @@
+package com.myapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class MyController {
+    @RequestMapping("api/health")
+    public String sayHello() {
+        return "API's Health is good";
+    }
+}
