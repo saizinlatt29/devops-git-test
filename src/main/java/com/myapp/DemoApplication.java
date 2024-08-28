0674d99fc03d6f7cb4146a0488dc39d8e3b74d44
[abc] <saizin.latt.it@gmail.com>
2024-08-28 09:22:29 -0700

    Testing finished

--- src/main/java/com/myapp/DemoApplication.java
+++ src/main/java/com/myapp/DemoApplication.java
@@ -3,6 +3,8 @@ package com.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DemoApplication {
 
